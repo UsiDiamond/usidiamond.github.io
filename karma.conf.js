2cd54d36646db18900c38dd4bd58eb8e0ba20c50
Usi Diamond <UsiDiamond@proton.me>
2023-11-10 22:18:46 -0500

    working coverage

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require("path").join(__dirname, "./coverage/usidiamond.github.io"),
+      dir: require("path").join(__dirname, "./coverage/usidiamond/"),
       subdir: ".",
       reporters: [{ type: "html" }, { type: "text-summary" }],
     },
