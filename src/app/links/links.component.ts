738ffba3a8ccbf4114019246e85e91ec2e78249b
Usi'ia Lior Diamond <usidiamond@proton.me>
2024-12-13 09:07:43 -0500

    more reorg

---
+++ src/app/links/links.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-links',
+  imports: [],
+  templateUrl: './links.component.html',
+  styleUrl: './links.component.css',
+})
+export class LinksComponent {}
