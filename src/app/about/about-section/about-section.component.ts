d9619537feddc98acbd01c800b37df9b7b608579
Usi'ia Lior Diamond <usidiamond@proton.me>
2025-01-01 21:41:04 -0500

    Fix About Flex

--- src/app/about/about-section/about-section.component.ts
+++ src/app/about/about-section/about-section.component.ts
@@ -1,10 +1,9 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-about-section',
+  selector: '[section]',
   imports: [],
   templateUrl: './about-section.component.html',
   styleUrl: './about-section.component.css',
 })
-export class AboutSectionComponent {
-}
+export class AboutSectionComponent {}
