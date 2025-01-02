c3c525a8cbeeb6a97c113f8b0422657048e7ee3e
Usi'ia Lior Diamond <usidiamond@proton.me>
2025-01-02 15:36:11 -0500

    Adjust Bootstrap Rendering

--- src/app/about/about-section/about-section.component.ts
+++ src/app/about/about-section/about-section.component.ts
@@ -3,6 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: '[section]',
   imports: [],
+  host: {
+    class: 'col-12 col-sm-12 col-md-5 col-lg-3 col-xl-3 mx-3 my-3 px-6 py-3',
+  },
   templateUrl: './about-section.component.html',
   styleUrl: './about-section.component.css',
 })
