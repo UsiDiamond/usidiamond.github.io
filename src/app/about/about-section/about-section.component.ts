4f0ba5ef3e1b0969356b63a97d5f57f7240635f1
Usi Diamond <28960829+UsiDiamond@users.noreply.github.com>
2025-01-02 16:12:10 -0500

    Fix Footer Spacing (#147)
    * Fix Footer Spacing
    
    * Make Uniform
    
    * Regularize Sections

--- src/app/about/about-section/about-section.component.ts
+++ src/app/about/about-section/about-section.component.ts
@@ -4,7 +4,7 @@ import { Component, Input } from '@angular/core';
   selector: '[section]',
   imports: [],
   host: {
-    class: 'col-12 col-sm-12 col-md-5 col-lg-3 col-xl-3 mx-3 my-3 px-6 py-3',
+    class: 'col-12 col-sm-12 col-md-5 col-lg-3 col-xl-3 m-1 p-3',
   },
   templateUrl: './about-section.component.html',
   styleUrl: './about-section.component.css',
