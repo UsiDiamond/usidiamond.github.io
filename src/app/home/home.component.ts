475b342e1fbf9a829e4222cc37036f7f472edd65
Usi'ia Lior Diamond <usidiamond@proton.me>
2025-01-05 09:07:36 -0500

    Simplify Real DOM

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'home',
+  selector: '[home]',
+  host: {
+    id: 'maincontent',
+    class: 'container mt-1 mb-5 backpane',
+    style: 'background-color: rgba(255, 255, 255, 0.096); border-radius: 25px;',
+  },
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone: false,
