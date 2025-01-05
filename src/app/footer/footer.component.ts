72d1a674ae0b3b117c08f44143248bd08745ce4a
Usi'ia Lior Diamond <usidiamond@proton.me>
2025-01-05 09:41:46 -0500

    Simplify DOM for Rest of Components

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: '[personal-footer]',
+  host: {
+    id: 'personal-footer',
+    class: 'container-fluid',
+  },
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
   standalone: false,
